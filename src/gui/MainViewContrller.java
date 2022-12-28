028527dd0bc43f4a1cf5b39fc97bd241f964bf30
smarcosm <sebastiaomarcos49@gmail.com>
2022-12-28 15:04:38 -0300

    SellerList

--- src/gui/MainViewContrller.java
+++ src/gui/MainViewContrller.java
@@ -17,6 +17,7 @@ import javafx.scene.control.MenuItem;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.VBox;
 import model.services.DepartmentService;
+import model.services.SellerService;
 
 public class MainViewContrller implements Initializable{
 	@FXML
@@ -28,7 +29,10 @@ public class MainViewContrller implements Initializable{
 	
 	@FXML
 	public void onMenuItemSellerAction() {
-		System.out.println("onMenuItemSellerAction");
+		loadView("/gui/SellerList.fxml", (SellerListController controller) -> {
+			controller.setSellerService(new SellerService());
+			controller.updateTableView();
+		});
 	}
 	@FXML
 	public void onMenuItemDepartmentAction() {
